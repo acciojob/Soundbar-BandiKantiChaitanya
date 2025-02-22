ffb23e96f0e75372abcec60674cd5bbc7f96d94a
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-22 14:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 // Store references to all buttons and the stop button
 const applauseBtn = document.querySelector('.btn:nth-of-type(1)');
 const booBtn = document.querySelector('.btn:nth-of-type(2)');
@@ -8,59 +7,50 @@ const victoryBtn = document.querySelector('.btn:nth-of-type(5)');
 const wrongBtn = document.querySelector('.btn:nth-of-type(6)');
 const stopBtn = document.querySelector('.stop');
 
-// Create audio elements for each sound
-const applauseSound = new Audio('sounds/applause.mp3');
-const booSound = new Audio('sounds/boo.mp3');
-const gaspSound = new Audio('sounds/gasp.mp3');
-const tadaSound = new Audio('sounds/tada.mp3');
-const victorySound = new Audio('sounds/victory.mp3');
-const wrongSound = new Audio('sounds/wrong.mp3');
+// Get the audio player
+const player = document.getElementById('player');
 
-// Function to stop all sounds
+// Function to stop the sound and reset it
 function stopAllSounds() {
-  applauseSound.pause();
-  applauseSound.currentTime = 0;
-  booSound.pause();
-  booSound.currentTime = 0;
-  gaspSound.pause();
-  gaspSound.currentTime = 0;
-  tadaSound.pause();
-  tadaSound.currentTime = 0;
-  victorySound.pause();
-  victorySound.currentTime = 0;
-  wrongSound.pause();
-  wrongSound.currentTime = 0;
+  player.pause();
+  player.currentTime = 0;
 }
 
 // Event listeners for buttons to play sounds
 applauseBtn.addEventListener('click', () => {
   stopAllSounds();
-  applauseSound.play();
+  player.src = 'sounds/applause.mp3';
+  player.play();
 });
 
 booBtn.addEventListener('click', () => {
   stopAllSounds();
-  booSound.play();
+  player.src = 'sounds/boo.mp3';
+  player.play();
 });
 
 gaspBtn.addEventListener('click', () => {
   stopAllSounds();
-  gaspSound.play();
+  player.src = 'sounds/gasp.mp3';
+  player.play();
 });
 
 tadaBtn.addEventListener('click', () => {
   stopAllSounds();
-  tadaSound.play();
+  player.src = 'sounds/tada.mp3';
+  player.play();
 });
 
 victoryBtn.addEventListener('click', () => {
   stopAllSounds();
-  victorySound.play();
+  player.src = 'sounds/victory.mp3';
+  player.play();
 });
 
 wrongBtn.addEventListener('click', () => {
   stopAllSounds();
-  wrongSound.play();
+  player.src = 'sounds/wrong.mp3';
+  player.play();
 });
 
 // Stop button functionality
